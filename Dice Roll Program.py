a72414040c7e57ecc85d64801f9b34afd3dac7cb
James Rocker <TheRockerfly@users.noreply.github.com>
2018-03-07 18:09:26 +0000

    Dice Roll
    Added a simple dice roll program which takes the number of dice a user wants to roll and how many times. The numbers will both be validated to determine if they are numbers and valid dice and the user will be prompted with a program restart if they want, or the program will close.

---
+++ Dice Roll Program.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  7 17:53:10 2018
+
+@author: James
+"""
+
+import random, time
+
+reroll = True
+valid_dice = [2, 3, 4, 5, 6, 7, 8, 10, 12, 14, 16, 18, 20, 24, 30, 34, 48, 50, 60, 100, 120]
+
+while reroll == True:
+    dice_roll = input("How many times do you want to roll a dice? ")
+    def dice_validation():
+        try:
+            int(dice_roll)
+            return True
+        except:
+            print("Please enter a number for the number of rolls")
+            
+    die_type = input("What kind of die do you want to roll? D")
+    def dice_type():
+        try:
+            int(die_type)
+            if int(die_type) in valid_dice:
+               return True
+            elif int(die_type) not in valid_dice: 
+               print("This is not a valid dice")
+               
+        except ValueError:
+            print("Please enter a number for the dice type input")
+
+    def die():
+        die = list(range(1, int(die_type) + 1))
+        return(die)
+
+    if dice_validation() == True and dice_type() == True:
+        for i in range(int(dice_roll)):
+            print(random.choice(die()))
+
+    time.sleep(3)
+    rerun = input("Do you want another roll? ").lower()
+    
+    run_true = ['yes','y','yeah']
+    run_false = ['no','n','nope']
+    
+    if rerun in run_true:
+        reroll = True
+        print("Okay, restarting the program.")
+        time.sleep(5)
+    elif rerun in run_false:
+        reroll = False
+        print("Okay, thank you for using the dice program.")
+        time.sleep(3)
+    else:
+        reroll = False
+        print("Sorry, I don't understand what you said. The dice program is shutting down.")
+        time.sleep(3)
