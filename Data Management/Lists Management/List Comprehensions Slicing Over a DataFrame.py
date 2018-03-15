78fd511ae413d4194d89e7b9d14352806be45d0d
TheRockerfly <jamesrocker@hotmail.co.uk>
2018-03-15 20:31:36 +0000

    Data Science Toolbox Update
    1. Added the certification 'Python Data Science Toolbox (Part 2) Course'
    2. Added the 'List Comprehension Slicing Over a DataFrame' py script
    3. Added the 'Conditional List Comprehensions' py script

---
+++ Data Management/Lists Management/List Comprehensions Slicing Over a DataFrame.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 15 14:15:06 2018
+
+@author: james
+"""
+import pandas as pd
+
+df = pd.read_csv('created_at')
+
+# Extract the created_at column from df: tweet_time
+tweet_time = df['created_at']
+
+# Extract the clock time: tweet_clock_time
+tweet_clock_time = [entry[11:19] for entry in tweet_time if entry[17:19] == '19']
+
+# Print the extracted times
+print(tweet_clock_time)
