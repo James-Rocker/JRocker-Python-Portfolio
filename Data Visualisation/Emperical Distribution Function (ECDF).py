d2741ceeb59d32a494e5816539475a60bb318d2a
James Rocker <TheRockerfly@users.noreply.github.com>
2018-03-23 08:29:55 +0000

    Update 31
    1. Added a function to calculate the emperical distribution for a graph
    2. Added the certification 'Merging DataFrames with pandas Course'

---
+++ Data Visualisation/Emperical Distribution Function (ECDF).py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 08:14:18 2018
+
+@author: James
+"""
+import numpy as np
+
+def ecdf(data):
+    n = len(data)
+    x = np.sort(data)
+    y = np.arange(1, n+1) / n
+    return x, y
