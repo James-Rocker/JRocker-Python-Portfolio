f06331b6a2f55da94dc30900e2e4c9b5fc409e15
James Rocker <TheRockerfly@users.noreply.github.com>
2018-03-24 08:25:46 +0000

    Update 33
    1. Added the 'Generating Randomised Graph Data' script to the data visualiation folder
    2. Minor update to the to-do list

---
+++ Data Visualisation/Generating Randomised Graph Data.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 24 06:42:35 2018
+
+@author: James
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Seed the random number generator and initalize
+np.random.seed(42)
+random_numbers = np.empty(100000)
+
+# Generate 100000 random numbers
+for i in range(100000):
+    random_numbers[i] = np.random.random()
+
+plt.hist(random_numbers)
+plt.show()
