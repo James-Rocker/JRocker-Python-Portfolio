188ab0fbf3dd9d671eea61df302deda3fecd2f3f
James Rocker <jamesrocker@hotmail.co.uk>
2020-03-08 16:14:11 +0000

    updated to a more generic function

--- Data Management/Loading Tables from SQLite.py
+++ Data Management/Loading Tables from SQLite.py
@@ -10,12 +10,14 @@ from sqlalchemy import create_engine
 from sqlalchemy import Column
 from sqlalchemy import Integer
 
-# Create an engine that connects to the census.sqlite file: engine
-engine = create_engine('sqlite:///census.sqlite', echo=True)
-metadata = MetaData()
 
-# Reflect census table from the engine: census
-census = Table('census', metadata,
-               Column('id', Integer, primary_key=True))
-metadata.create_all(engine)
+def create_micro_db(table_name: str):
+    # Create an engine that connects to the census.sqlite file: engine
+    engine = create_engine('sqlite:///micro_db.sqlite', echo=True)
+    metadata = MetaData()
+
+    # Reflect the selected table from the engine: micro_db
+    census = Table(table_name, metadata,
+                   Column('id', Integer, primary_key=True))
+    metadata.create_all(engine)
 
