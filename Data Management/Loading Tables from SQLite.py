79454033e1861d1cbeb741a84afeb34bec0edd62
James Rocker <jamesrocker@hotmail.co.uk>
2020-03-08 16:11:53 +0000

    fixed so it will actually work now

--- Data Management/Loading Tables from SQLite.py
+++ Data Management/Loading Tables from SQLite.py
@@ -1,21 +1,21 @@
-# -*- coding: utf-8 -*-
 """
 Created on Tue Mar 13 08:27:33 2018
 
 @author: James
 """
 
-# Import Table
 from sqlalchemy import Table
 from sqlalchemy import MetaData
 from sqlalchemy import create_engine
+from sqlalchemy import Column
+from sqlalchemy import Integer
 
 # Create an engine that connects to the census.sqlite file: engine
-engine = create_engine('sqlite:///census.sqlite')
+engine = create_engine('sqlite:///census.sqlite', echo=True)
 metadata = MetaData()
 
 # Reflect census table from the engine: census
-census = Table('census', metadata, autoload=True, autoload_with=engine)
+census = Table('census', metadata,
+               Column('id', Integer, primary_key=True))
+metadata.create_all(engine)
 
-# Print census table metadata
-print(repr(census))
