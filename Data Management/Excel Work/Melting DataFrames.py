2a999778e35b14aeeb1e594a9db70a513705f462
James Rocker <jamesrocker@hotmail.co.uk>
2020-03-08 16:20:56 +0000

    attempt to make it more functional

--- Data Management/Excel Work/Melting DataFrames.py
+++ Data Management/Excel Work/Melting DataFrames.py
@@ -6,27 +6,28 @@ Created on Sat Mar 17 06:59:31 2018
 """
 import pandas as pd
 
-filename = ''
-df = pd.read_csv(filename)
 
-# Melting
-# Reset the index: visitors_by_city_weekday
-df_reset = df.reset_index() 
+def single_melt_csv_df(file_name: str, melt_col: str, val_col: str):
+    df = pd.read_csv(file_name)
 
-# Print visitors_by_city_weekday
-print(df_reset)
+    # Reset the index
+    df_reset = df.reset_index()
 
-# Melt visitors_by_city_weekday: visitors
-df_melt = pd.melt(df_reset, id_vars=[''], value_name='')
+    # Melt visitors_by_city_weekday: visitors
+    df_melt = pd.melt(df_reset, id_vars=[melt_col], value_name=val_col)
 
-# Print visitors
-print(df_melt)
+    # Print visitors
+    print(df_melt)
 
-# Multiple columns can be used as identifiers
-skinny = pd.melt(df, id_vars=['', ''])
 
-# Print skinny
-print(skinny)
+def multi_col_melt(file_name, melt_list: list):
+    df = pd.read_csv(file_name)
+    # Multiple columns can be used as identifiers
+    skinny = pd.melt(df, id_vars=melt_list)
 
-# Key-value pairs merge multiindex columns into one
-kv_pairs = pd.melt(df, col_level=0)
+    # Print skinny
+    print(skinny)
+
+    # Key-value pairs merge multi-index columns into one
+    kv_pairs = pd.melt(df, col_level=0)
+    print(kv_pairs)
