6a70d9d6edfcf34625f0795061384efda2427ff1
James Rocker <james.rocker@simplybusiness.co.uk>
2021-07-02 11:23:36 +0100

    fix the azure datafactory.py naming and imports

--- Azure/datafactory.py
+++ Azure/datafactory.py
@@ -1,9 +1,6 @@
 from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.datafactory import DataFactoryManagementClient
-from azure.mgmt.datafactory.models import *
-from datetime import datetime, timedelta
-import time
 
 
 def print_item(group):
