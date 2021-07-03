db434d7743935b4783be13f097db30ed4bd9aee0
TheRockerfly <jamesrocker@hotmail.co.uk>
2021-07-03 14:40:23 +0100

    add mumbling.py

---
+++ technical_exercises/mumbling.py
@@ -0,0 +1,10 @@
+
+def accumulate(s):
+    list_of_letters = list(s.replace(" ", ""))
+    output_string = ''
+    for count, letter in enumerate(list_of_letters):
+        output_string += letter.upper()
+        if count > 0:
+            output_string += count * letter.lower()
+        output_string += "-"
+    return output_string[:-1]
