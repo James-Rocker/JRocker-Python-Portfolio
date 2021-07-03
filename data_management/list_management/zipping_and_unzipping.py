4e70fc81440151b39ceb7b3b349ad3f6ca4994d0
James Rocker <james.rocker@simplybusiness.co.uk>
2021-07-03 16:03:20 +0100

    improved some of the wording and the file name

--- data_management/list_management/zipping_and_unzipping.py
+++ data_management/list_management/zipping_and_unzipping.py
@@ -19,7 +19,7 @@ data2 = ("bald", "unknown", "blue", "german?", "spooky")
 z1 = zip(data1, data2)
 
 # Print the tuples in z1 by unpacking with *
-print(*z1)
+print(f"Unpacked zipped objects - {*z1,}")
 
 # Unpacking a zipped object exhausts the zipped object
 # Re-create a zip object from data1 and dat2: z1
@@ -28,9 +28,7 @@ z1 = zip(data1, data2)
 # 'Unzip' the tuples in z1 by unpacking with * and zip(): result1, result2
 # This assigns the 1st list of data to result1 and the 2nd list to result2
 result1, result2 = zip(*z1)
-print(result1)
-print(data1)
 
 # Check if unpacked tuples are equivalent to original tuples
-print(result1 == data1)
-print(result2 == data2)
+if result1 == data1 and result2 == data2:
+    print(f'Uncoupled objects {result1, result2}')
