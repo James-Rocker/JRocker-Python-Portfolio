10456c2f5e4867b88f80d17d66e42725e63b211f
TheRockerfly <jamesrocker@hotmail.co.uk>
2021-07-06 21:13:25 +0100

    make readable time

---
+++ technical_exercises/time_for_humans.py
@@ -0,0 +1,14 @@
+def make_readable(seconds: int):
+    if seconds <= 359999:
+        hours = seconds // (60*60)
+        minutes = seconds % (60*60) // 60
+        seconds = seconds % 60
+        return '{:02}:{:02}:{:02}'.format(hours, minutes, seconds)
+    else:
+        return "99:59:59"
+
+
+print(make_readable(5))
+print(make_readable(3500))
+print(make_readable(359998))
+print(make_readable(9000000))
