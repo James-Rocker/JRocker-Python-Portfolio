5f6bd121fc5971257f82414287757de619e7b0a2
james.rocker <james.rocker@sainsburys.co.uk>
2022-11-27 18:07:32 +0000

    callables

---
+++ object_oriented_programming/duck_types/callable.py
@@ -0,0 +1,33 @@
+
+"""
+The __call__ method can be used to turn the instances of the class into callables. Functions are callable objects
+by defining the __call__ method for a class, we can
+
+this is done by using the `callable which checks if the object is callable or not
+"""
+
+
+class B:
+    def __init__(self, v):
+        self.v = v
+
+
+beep = B(10)
+try:
+    print(beep())
+except TypeError as e:
+    print(e, e.__traceback__)
+
+print("interesting, let's see. Is it callable?", callable(beep))
+
+
+class A:
+    def __init__(self, v):
+        self.v = v
+
+    def __call__(self):
+        return f"{self.v} is the value we are calling"
+
+
+thing = A(10)
+print("Is this callable?", callable(thing), thing())
