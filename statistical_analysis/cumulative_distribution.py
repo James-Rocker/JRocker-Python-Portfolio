bd03a51e6bc60e93eb07c47ba207323e2a21961e
james.rocker <james.rocker@sainsburys.co.uk>
2022-11-29 13:12:41 +0000

    add more details and some explanations I wish I had made at the time of originally writing

--- statistical_analysis/cumulative_distribution.py
+++ statistical_analysis/cumulative_distribution.py
@@ -10,17 +10,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from empirical_distribution_function import empirical_dist
 
-# Creates 3 numpy with a mean (centre) of 20 and a variety of standard deviation scale
-# with a sample size of 100000
+"""
+Let's try and see the difference of scale numpy makes when building a random size array
+
+Creates 3 numpy arrays with a mean (centre) of 20 and a variety of standard deviation scale
+with a sample size of 100000 and variety of scales
+"""
 samples_std1 = np.random.normal(20, 1, 100000)
 samples_std3 = np.random.normal(20, 3, 100000)
 samples_std10 = np.random.normal(20, 10, 100000)
 
-# Generate CDFs
+# Generate the
 x_std1, y_std1 = empirical_dist(samples_std1)
 x_std3, y_std3 = empirical_dist(samples_std3)
 x_std10, y_std10 = empirical_dist(samples_std10)
 
+# Let's plot these values
 plt.plot(x_std1, y_std1, marker=".", linestyle="none")
 plt.plot(x_std3, y_std3, marker=".", linestyle="none")
 plt.plot(x_std10, y_std10, marker=".", linestyle="none")
--- statistical_analysis/empirical_distribution_function.py
+++ statistical_analysis/empirical_distribution_function.py
@@ -9,14 +9,15 @@ from typing import Any, Tuple
 import numpy as np
 
 
-def empirical_dist(data: np.ndarray[Any, Any]) -> Tuple:  # pylint: disable=E1136
+def empirical_dist(data: np.ndarray[Any, Any]) -> Tuple:
     """
     Takes a numpy array arg, creates a sorted array as the x_axis and y_axis for a numpy array
-    Then return the two numpy arrays as a tuple
+    Then return the two numpy arrays as a tuple object to split it later
     """
+    # we use numpy size method because it calculates total number of elements vs len which is just the first dimension
     list_count = data.size
-    x_axis = np.sort(data)
+    x_axis = np.sort(data)  # we want to graph this data later, so we need this sort
 
-    # we are dividing here because it makes visualising larger arrays easier
-    y_axis = np.arange(1, list_count + 1) / list_count
+    # returns evenly spaced values within a given interval. These should be ints, for decimals use np.linspace
+    y_axis = np.arange(start=1, stop=list_count + 1) / list_count  # divide here, so we always keep the y-axis max of 1
     return x_axis, y_axis
