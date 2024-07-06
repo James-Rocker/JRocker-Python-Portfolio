328504b863481284d1e333feeeee93c3b82ef505
james <jamesrocker@hotmail.co.uk>
2024-07-06 09:13:01 +0100

    sorted, styled labelled and title

--- data_visualisation/seaborn_box_and_whiskers.py
+++ data_visualisation/seaborn_box_and_whiskers.py
@@ -12,12 +12,28 @@ import pandas as pd
 # load the example dataset
 df = sns.load_dataset('tips')
 
-# Create box plot with Seaborn's default settings
-sns.boxplot(data=df, x='day', y='total_bill')
+# Let's inspect the data
+print(df.head())
+
+# set the graph style. List of possible choices comes from the `set_style` function in `rcmod.py`
+sns.set_style(style="dark")
+# set the palette. List of possible choices comes from the color_palette function in seaborn `palettes.py`
+sns.set_palette(palette="deep")
+
+# We want to sort the day order by the most amount of sales. We need another dataframe to represent that
+total_cost = df[["total_bill", "day"]]
+total_cost = total_cost.groupby(by=["day"])["total_bill"].sum().reset_index()
+sorted_total_cost = total_cost.sort_values("total_bill", ascending=False)
+
+# Create box plot with Seaborn's default settings. Let's also compare the smokers as well
+sns.boxplot(data=df, x='day', y='total_bill', hue="smoker", order=sorted_total_cost["day"])
 
 # Label the axes
 plt.xlabel("day")
 plt.ylabel("total bill")
+
+# setting the title
+plt.title('Tips by Day')
 
 # Show the plot, disabling here because I'm using a headless console
 # plt.show()
