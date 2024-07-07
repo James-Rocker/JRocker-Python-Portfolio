a43181258a14b424e276a1f3c68b81b1d1057dc8
james <jamesrocker@hotmail.co.uk>
2024-07-07 15:04:44 +0100

    updating this as the command no longer worked

--- data_management/create_sqlite_engine.py
+++ data_management/create_sqlite_engine.py
@@ -4,13 +4,18 @@ Created on Tue Mar 13 08:20:22 2018
 @author: James
 """
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 
 
 def get_tables_in_db():
     # Create an engine that connects to the census.sqlite file: engine
     engine = create_engine("sqlite:///census.sqlite")
 
+    # using the slightly newer run time inspection module
+    engine_inspection = inspect(engine)
+    table_names = engine_inspection.get_table_names()
     # Print table names
-    print(engine.table_names())
-    return engine.table_names()
+    return table_names
+
+
+print(get_tables_in_db())
