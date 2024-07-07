9308179cd1eff9d473243e581682a56bad5d9f0f
james <jamesrocker@hotmail.co.uk>
2024-07-07 15:34:43 +0100

    fixed up swarmplot to use the penguin dataset

--- data_visualisation/seaborn_swarmplot.py
+++ data_visualisation/seaborn_swarmplot.py
@@ -1,23 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar 11 16:05:55 2018
+Created on Wed Mar 21 07:34:36 2018
 
-@author: james
+@author: James
 """
-
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pandas as pd
 
-example = {
-    "Column 1": [1, 2, 5, 3, 7],
-    "Column 2": [5, 7, 1, 2, 5],
-    "Column 3": [7, 2, 5, 4, 1],
-}
-example = pd.DataFrame(example, index=["A", "B", "C", "D", "E"])
+# Load the penguins dataset from Seaborn:
+penguins = sns.load_dataset('penguins')
 
-# Create violin plot
-sns.swarmplot(x=example.index, y="Column 3", data=example)
+# Create a swarm plot of flipper length vs species
+sns.swarmplot(x="flipper_length_mm", y="species", data=penguins)
 
 # Show the plot, disabling here because I'm using a headless console
 # plt.show()
